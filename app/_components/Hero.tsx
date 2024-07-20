9a8e6d20c152a03f576dbaede2d1581b2bec0970
chaituabhade <156505752+chaituabhade@users.noreply.github.com>
2024-07-20 10:35:46 +0530

    Update Hero.tsx

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,45 +1,200 @@
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { ArrowRight } from "lucide-react";
-import React from "react";
+"use client"
+import Image from "next/image";
+import Header from "./_components/Header";
+//import Hero from "./_components/Hero";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect } from "react";
+import MaxWidthWrapper from '../components/MaxWidthWrapper'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
+import { buttonVariants } from '@/components/ui/button'
 
-const Hero = () => {
+
+export default function Home() {
+
+  const { user } = useKindeBrowserClient();
+
   return (
-    <section className="relative bg-gradient-to-b from-neutral-900 via-gray-900 to-neutral-900 text-white">
-      <div className="absolute top-28 w-full justify-center flex ">
-        <div className="py-2 px-2 rounded-3xl border-t-[1.5px] border-l-2 border-white/20  bg-white/20 cursor-pointer shadow-lg">
-          <p className="mx-auto text-white text-sm">
-            See what's new |{" "}
-            <span className="text-sky-500">AI Diagrams {">"} </span>
+    <>
+      <Header />
+      <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
+        <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
+          <p className='text-sm font-semibold text-gray-700'>
+            See What's New | <span className='text-sky-300'>AI Diagram</span>
           </p>
         </div>
-      </div>
-      <div className="mx-auto max-w-screen-xl px-8 py-32 lg:flex lg:h-screen lg:items-center">
-        <div className="mx-auto max-w-3xl mt-8 text-center">
-          <h1 className="bg-gradient-to-r from-blue-300 via-blue-500 to-purple-600 bg-clip-text p-2 text-3xl font-extrabold text-transparent sm:text-5xl">
-            Documents & diagrams
-          </h1>
-          <p className="sm:block text-5xl font-semibold">
-            {" "}
-            for engineering teams{" "}
-          </p>
+        <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+          <span className='text-blue-600'>Eraser,</span>{' '}
+          Instant Canvas for Your Imagination.
+        </h1>
+        <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+          Eraser allows you to bring your ideas to life on a digital canvas. Simply start creating, and watch your imagination take shape instantly.
+        </p>
 
-          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-            All-in-one markdown editor, collaborative canvas, and
-            diagram-as-code builder
-          </p>
+        <Link
+          className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5',
+          })}
+          href='/dashboard'
+          target='_blank'>
+          Get started{' '}
+          <ArrowRight className='ml-2 h-5 w-5' />
+        </Link>
+      </MaxWidthWrapper>
 
-          <div className="mt-8 flex flex-wrap justify-center gap-4">
-            <LoginLink className="flex items-center w-full rounded-lg border border-white bg-neutral-100 px-4 py-2 text-sm font-medium text-black hover:bg-neutral-300 hover:text-neutral-900 focus:outline-none focus:ring active:text-opacity-75 sm:w-auto">
-              Get Started
-              <p className="ml-2">
-                <ArrowRight size={24} />
-              </p>
-            </LoginLink>
+      {/* value proposition section */}
+      <div>
+        <div className='relative isolate'>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+
+          <div>
+            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+              <div className='mt-16 flow-root sm:mt-24'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                  <Image
+                    src='/eraser photo.png'
+                    alt='product preview'
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+            />
           </div>
         </div>
       </div>
-    </section>
-  );
-};
 
-export default Hero;
+      {/* Feature section */}
+      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+        <div className='mb-12 px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Start creating
+            </h2>
+            <p className='mt-4 text-lg text-gray-600'>
+              Bringing your ideas to life has never been easier than with Eraser. Start creating on your digital canvas in minutes.
+            </p>
+          </div>
+        </div>
+
+        {/* steps */}
+        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 1
+              </span>
+              <span className='text-xl font-semibold'>
+                Sign up for an account
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                It's totaly FREE to use.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 2
+              </span>
+              <span className='text-xl font-semibold'>
+                Make a team and start
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Invite team members.
+              </span>
+            </div>
+          </li>
+          <li className='md:flex-1'>
+            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+              <span className='text-sm font-medium text-blue-600'>
+                Step 3
+              </span>
+              <span className='text-xl font-semibold'>
+                Make a file
+              </span>
+              <span className='mt-2 text-zinc-700'>
+                Make as many as files and give way to yor creativity and work.
+              </span>
+            </div>
+          </li>
+        </ol>
+
+        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+          <div className='mt-16 flow-root sm:mt-24'>
+            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+              <Image
+                src='/dashbordpic.png'
+                alt='uploading preview'
+                width={1419}
+                height={732}
+                quality={100}
+                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='mb-12 px-6 py-40 lg:px-8'>
+          <div className='mx-auto max-w-2xl sm:text-center'>
+            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+              Make digrams with code
+            </h2>
+            
+          </div>
+        </div>
+
+        <div>
+          <p className=' px-60 mt-4 text-gray-600'>
+                Download the image for better undestanding of code <span className="text-blue-400"><a href="eraser photo1.PNG" download="Download">Here.</a></span>
+          </p>
+          <div className='mx-auto max-w-9xl px-6  lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  src='/eraser photo1.PNG'
+                  alt='product preview'
+                  width={1500}
+                  height={1000}
+                  quality={100}
+                  className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+          <p className=' px-60  mt-8 text-gray-600'>
+                Made by Chaitanya Abhade visit github for more info  <span className="text-blue-400"><a href="https://github.com/chaituabhade" >GITHUB</a></span>
+          </p>
+        </div>
+
+      </div>
+    </>
+  )
+
+}
+export default Hero
